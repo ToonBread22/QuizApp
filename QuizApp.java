11a440a45744cf78016ede48bc55fdab1f3dc4f1
ToonBread22 <71267390+ToonBread22@users.noreply.github.com>
2020-09-14 08:42:10 -0400

    initial

---
+++ QuizApp.java
@@ -0,0 +1,13 @@
+package quizapp;
+
+/**
+ *
+ * @author Jonah Meyer-Crothers
+ */
+public class QuizApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
